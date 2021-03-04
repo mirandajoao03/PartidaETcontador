18241bdea4c665af9fa64a576e1512923c08ee9b
20185561 <20185561@DSN1271158622.cfp127aluno.local>
2021-03-04 14:02:16 -0300

    partida ET com contontador acionado por botão

---
+++ partidaETcontador.X/display7seg.c
@@ -0,0 +1,44 @@
+/*
+ * File:   display7seg.c
+ * Author: 20185561
+ *
+ * Created on 24 de Fevereiro de 2021, 15:48
+ */
+
+
+#include <xc.h>
+#include "config.h"
+
+char vetor[16] = { 0x3F, 0x06, 0x5B, 0x4F,
+                   0x66, 0x6D, 0x7D, 0x07,
+                   0x7F, 0x6F, 0x77, 0x7C,
+                   0x39, 0x5E, 0x79, 0x71 };
+
+void display7seg_init ( void )
+{
+    ANSELH = 0;
+    TRISB = 0x00;
+    PORTB = 0x00;
+}
+
+void display7seg ( int c )
+{
+    PORTB = vetor[c];
+}
+
+void botoes ( void )
+{
+    TRISDbits.TRISD3 = 1;
+    TRISDbits.TRISD2 = 1;
+}
+
+int botao1 ( void )
+{
+    return ( PORTDbits.RD3 );
+}
+
+int botao0 ( void )
+{
+    return ( PORTDbits.RD2 );
+}
+
---
+++ partidaETcontador.X/main.c
@@ -0,0 +1,81 @@
+/*
+ * File:   main.c
+ * Author: 20185561
+ *
+ * Created on 3 de Março de 2021, 16:14
+ */
+
+
+#include <xc.h>
+#include "config.h"
+#include "delay.h"
+#include "contatores.h"
+#include "botoes.h"
+#include "display7seg.h"
+
+void main(void) 
+{
+    int cont = 0;
+    int estado = 0;
+    int t;
+    
+    while ( 1 )
+    {    
+        switch ( estado )
+        {
+            case 0:
+                    estado = 1;
+                    break;
+            case 1:
+                    contatores_init();
+                    botoes_init();
+                    display7seg_init();
+                    estado = 2;
+                    break;
+            case 2:
+                    if ( S1() == 1 && K1status() == 0 )
+                        estado = 3;
+                    break;
+            case 3:
+                    K1(1);
+                    K2(1);
+                    K3(0);
+                    estado = 4;
+                    break;
+            case 4:
+                    t = 3000;
+                    estado = 6;
+                    break;
+            case 6:
+                    delay(1);
+                    --t;
+                    if (t <= 0 )
+                        estado = 7;
+                    if( S0() == 1 )
+                        estado = 9;
+                    break;
+            case 7:
+                    ++cont;
+                    estado = 8;
+                    break;
+            case 8:
+                    K1(1);
+                    K2(0);
+                    K3(1);
+                    if( S0() == 1 )
+                        estado = 9;
+                    break;
+            case 9:
+                    K1(0);
+                    K2(0);
+                    K3(0);
+                    estado = 2;
+                    break;
+        }      
+        display7seg( cont );
+        
+        if ( cont >= 10)
+            cont = 0;
+
+    }    
+}
